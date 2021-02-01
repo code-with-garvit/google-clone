ca33019a9b86d5e1a454c29c44bfb35be7386988
Garvit Motwani <garvitmotwani02@gmail.com>
2021-02-01 17:02:53 +0530

    Google Clone

---
+++ src/API.js
@@ -0,0 +1,4 @@
+export const key = "AIzaSyDmbUlcKtoCJ9ZNz9d7ZSs4yhG7ldkAExY";
+export const cx = "efd50a6e75f732ea3";
+
+// You Can Use This API Key but I would rather prefer you to get one of your own
