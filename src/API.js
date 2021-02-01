acfa4414354b8939f7f17cca6fddd6ba8b202617
Garvit Motwani <63104084+code-with-garvit@users.noreply.github.com>
2021-02-01 21:42:00 +0530

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,4 +1,2 @@
-export const key = "AIzaSyDmbUlcKtoCJ9ZNz9d7ZSs4yhG7ldkAExY";
-export const cx = "efd50a6e75f732ea3";
-
-// You Can Use This API Key but I would rather prefer you to get one of your own
+export const key = "your API key here";
+export const cx = "your cx here";
